b941645b9c7a4f06c01b4fd689b38313c04bf648
Krisnaabdi <abdidevinjer@gmail.com>
2021-03-09 15:28:11 +0700

    mulai project

--- calculator.cs
+++ calculator.cs
@@ -2,7 +2,25 @@ class Calculator
 {
     static void Main(string[] args)
 	{
+		int a = 10;
+		int b = 6;
+		
+		console.writeline("Hasil penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
+		console.writeline("Hasil Pengurangan: {0} - {1} ={2}", a, b, Pengurangan(a, b));
+		
+		console.writeline("\nTekan sembarang key untuk keluar");
+		console.readkey();
 	
+	}
+	
+	static int penambahan(inta, int b)
+	{
+		return a + b;
+	}
+	
+	static int pengurangan(int a, int b)
+	{
+		return a - b;
 	}
 	
 }
